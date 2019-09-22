37c46c1ced15e27b8ac2a81d149c3037c366c18b
Jean-David BARBIER <jean-david.barbier@tohero.fr>
2019-09-22 14:08:10 +0200

    Ajout effets menu + revue blocs sujets

--- integration-html/js/script.js
+++ integration-html/js/script.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
         $( this ).find( ".details").fadeToggle( "show hide", "linear" );
     });
 
+    $('.nav-item').on('show.bs.dropdown', function () {
+        $(".overlay").addClass("visible");
+        $( this ).parent().parent().parent().addClass("menu-deployed");
+    });
+
+    $('.nav-item').on('hide.bs.dropdown', function () {
+        $(".overlay").removeClass("visible");
+        $( this ).parent().parent().parent().removeClass("menu-deployed");
+    });
 });
